df181528c5414bace840090edd1c1c7199e851d8
Timodruid <dxleotimox100pre@gmail.com>
2023-12-09 19:50:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// Ejercicio 1
+function square(number) {
+    return number * number;
+  } 
+ console.log(square(10));
+
+// Ejercicio 2
+// const divideByTwo = (number) => number/2;
+// console.log(divideByTwo(50));
+
+// Ejercicio 3
+// let student={
+//     firstName: "Timoteo",
+//     lastName: "Vergara",
+//     age: "25",
+//     hubby: "videojuegos",
+// }
+// console.log(student)
+
+// Ejercicio 4
+// function getTipoDatos(array) {
+//   return array.map(elem => typeof elem);
+// }
+// let originalArray = [function(){}, true];
+// let resultArray = getTipoDatos(originalArray);
+// console.log(resultArray);
+
+// Ejercicio 5
+// const person={
+//     head: {
+//         eyes: '8',
+//         mouth: {
+//             teeth: '7',
+//             tongue: '4'
+//         }
+//     },
+//     body: {
+//         shoulders: '10',
+//         chest: '9',
+//         arms: '8.5',
+//         hands: '9',
+//         legs: '10'
+//     }
+// }
+// console.log(person.head.eyes);
+// console.log(person.body.legs);
+
+// Ejercicio 6
+// let Name = "Timoteo";
+// let FirstName = "timo";
+// let LastName = "Vergara";
+// let Age = 25;
+// let hubby = "videojuegos";
+
+// console.log(`Mi nombre es ${Name}. Yo tengo ${Age} años. Mi apodo es ${FirstName},
+// Mi apellido es ${LastName}, y mi aficion son los ${hubby}`);
+
+// Ejercicio 7
+// let Edad = 15;
+// let Status = Edad >= 18 ? "Adulto" : "Menor";
+
+// console.log(`La persona es ${Status}`);
+
+// Ejercicio 8
+// import { pi } from './date.js';
+// import { career } from './profession';
+// console.log(pi);
+// console.log(career);
